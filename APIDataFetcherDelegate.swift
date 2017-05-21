f9045a2a8b54e6a24c48769c689a6558ccaae59d
Ambuj Punn <ambujpunn@gmail.com>
2017-05-20 19:50:33 -0700

    delegate method for data

---
+++ APIDataFetcherDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  APIDataFetcherDelegate.swift
+//  Excursion
+//
+//  Created by Ambuj Punn on 11/10/16.
+//
+//
+
+import Foundation
+
+@objc protocol APIDataFetcherDelegate: class {
+    func didFetchData(data: Array<Any>?) -> Void
+    func didReturnError(error: Error) -> Void
+    @objc optional func didLoadNextPage(data: Array<Any>?) -> Void
+}
